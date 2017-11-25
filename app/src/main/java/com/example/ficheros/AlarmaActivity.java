8d418b066dec5b5fe90615be882438dcc0ea9ef3
JuanFranCrater <juanfbenitezlopez@gmail.com>
2017-11-25 13:46:28 +0100

    0

--- app/src/main/java/com/example/ficheros/AlarmaActivity.java
+++ app/src/main/java/com/example/ficheros/AlarmaActivity.java
@@ -1,5 +1,7 @@
 package com.example.ficheros;
 
+import android.app.AlarmManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -31,6 +33,8 @@ public class AlarmaActivity extends AppCompatActivity implements View.OnClickLis
 
     Memoria miMemoria;
 
+    Bundle bnd;
+
     private static final String RUTA = "alarmas.txt";
 
     private static final String CODIFICACION = "UTF-8";
@@ -42,6 +46,7 @@ public class AlarmaActivity extends AppCompatActivity implements View.OnClickLis
         inicializar();
     }
     void inicializar() {
+       bnd = new Bundle();
         edtMin1 = (EditText) findViewById(R.id.edtMinAl1);
         edtMen1 = (EditText) findViewById(R.id.edtMenAl1);
         edtMin2 = (EditText) findViewById(R.id.edtMinAl2);
@@ -61,33 +66,76 @@ public class AlarmaActivity extends AppCompatActivity implements View.OnClickLis
         if(view ==btnIniciar)
         {
             Guardar();
+            Iniciar();
         }
     }
+
+    private void Iniciar() {
+        Intent intnt = new Intent(AlarmaActivity.this, ContadorActivity.class);
+        intnt.putExtra("alarmas",bnd);
+        startActivity(intnt);
+    }
+
     private void Guardar()
     {
         if (edtMin1.getText().length() == 0 && edtMin2.getText().length() == 0 && edtMin3.getText().length() == 0 && edtMin4.getText().length() == 0 && edtMin5.getText().length() == 0) {
             Toast.makeText(this, "Rellene los campos incompletos", Toast.LENGTH_SHORT).show();
         }
         else {
-            if(edtMen1.getText().length()==0) {
+            if(edtMin1.getText().length()>0&&edtMin1.getText().length()<3) {
                 alarma1 = edtMin1.getText().toString() + "; " + edtMen1.getText().toString() + "\n";
                 miMemoria.escribirExterna(RUTA, alarma1, true, CODIFICACION);
+                bnd.putInt("Alarma1",Integer.valueOf(edtMin1.getText().toString()));
+                if (edtMen1.getText().length()>0)
+                {
+                    bnd.putString("Alarma1",edtMen1.getText().toString());
+                }else{
+                    bnd.putString("Alarma1","Mensaje alarma 1");
+                }
             }
-            if(edtMen2.getText().length()==0) {
+            if(edtMin2.getText().length()>0&&edtMin2.getText().length()<3) {
             alarma2 = edtMin2.getText().toString() + "; " + edtMen2.getText().toString()  + "\n";
             miMemoria.escribirExterna(RUTA, alarma2, true, CODIFICACION);
+                bnd.putInt("Alarma2",Integer.valueOf(edtMin2.getText().toString()));
+                if (edtMen2.getText().length()>0)
+                {
+                    bnd.putString("Alarma2",edtMen2.getText().toString());
+                }else{
+                    bnd.putString("Alarma2","Mensaje alarma 2");
+                }
             }
-            if(edtMen3.getText().length()==0) {
+            if(edtMin3.getText().length()>0&&edtMin3.getText().length()<3) {
             alarma3 = edtMin3.getText().toString() + "; " + edtMen3.getText().toString()  + "\n";
             miMemoria.escribirExterna(RUTA, alarma3, true, CODIFICACION);
+                bnd.putInt("Alarma3",Integer.valueOf(edtMin3.getText().toString()));
+                if (edtMen3.getText().length()>0)
+                {
+                    bnd.putString("Alarma3",edtMen3.getText().toString());
+                }else{
+                    bnd.putString("Alarma3","Mensaje alarma 3");
+                }
             }
-            if(edtMen4.getText().length()==0) {
+            if(edtMin4.getText().length()>0&&edtMin4.getText().length()<3) {
             alarma4 = edtMin4.getText().toString() + "; " + edtMen4.getText().toString()  + "\n";
             miMemoria.escribirExterna(RUTA, alarma4, true, CODIFICACION);
+                bnd.putInt("Alarma4",Integer.valueOf(edtMin4.getText().toString()));
+                if (edtMen4.getText().length()>0)
+                {
+                    bnd.putString("Alarma4",edtMen4.getText().toString());
+                }else{
+                    bnd.putString("Alarma4","Mensaje alarma 4");
+                }
             }
-            if(edtMen5.getText().length()==0) {
+            if(edtMin5.getText().length()>0&&edtMin5.getText().length()<3) {
             alarma5 = edtMin5.getText().toString() + "; " + edtMen5.getText().toString()  + "\n";
             miMemoria.escribirExterna(RUTA, alarma5, true, CODIFICACION);
+                bnd.putInt("Alarma5",Integer.valueOf(edtMin5.getText().toString()));
+                if (edtMen5.getText().length()>0)
+                {
+                    bnd.putString("Alarma5",edtMen5.getText().toString());
+                }else{
+                    bnd.putString("Alarma5","Mensaje alarma 1");
+                }
             }
                 Toast.makeText(this, "Alarmas Guardadas", Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/com/example/ficheros/ConversorActivity.java
+++ app/src/main/java/com/example/ficheros/ConversorActivity.java
@@ -1,13 +1,88 @@
 package com.example.ficheros;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class ConversorActivity extends AppCompatActivity {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class ConversorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Instanciamos los objetos de editText, un radio button(para comprobar el tipo de cambio) y el boton (para el metodo onClick)
+    Button boton;
+    RadioButton rboton;
+    EditText edText;
+    EditText edText2;
+    String cambio;
+    final String url = "http://alumno.mobi/~alumno/superior/benitez/cambio.txt";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversor);
+        boton =(Button) findViewById(R.id.botonConvertir);
+        boton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //cambio=
+        rboton=(RadioButton) findViewById(R.id.rbEaD);
+        if (rboton.isChecked())
+        {
+            edText = (EditText) findViewById(R.id.etDolar);
+            edText2 = (EditText) findViewById(R.id.etEuro);
+            try {
+                edText.setText(convertirADolares(edText2.getText().toString(),cambio));
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }else
+        {
+            edText = (EditText) findViewById(R.id.etEuro);
+            edText2 = (EditText) findViewById(R.id.etDolar);
+
+            try {
+                edText.setText(convertirAEuros(String.format(edText2.getText().toString()),cambio));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String convertirADolares(String cantidad,String cambio) {
+        double valor =
+                Double.parseDouble(cantidad) / Double.parseDouble(cambio);
+        return
+                Double.toString(valor);
+    }
+
+    public String convertirAEuros(String cantidad,String cambio) {
+        double valor =
+                Double.parseDouble(cantidad) * Double.parseDouble(cambio);
+        return
+                Double.toString(valor);
     }
 }
